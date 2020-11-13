938fd1aef4cfcf80906f1a3cf765babdda271030
Victor Trincado <v.trincado@didacware.ch>
2020-11-13 16:50:38 +0100

    Productlist

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album} from './album';
-
+import { Product } from "./product";
 
 @Injectable(
 )
@@ -11,10 +11,16 @@ import { Album} from './album';
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
+
   constructor(private _http: Http) { }
 
   getAlbum(id:number) : Observable<Album>{
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
