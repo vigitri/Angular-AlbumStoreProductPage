49ee43388446a0018ef4e3da405892adc284815a
Victor Trincado <v.trincado@didacware.ch>
2020-11-13 15:09:11 +0100

    Interface Album and track

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album} from './album';
 
 
 @Injectable(
@@ -11,8 +13,8 @@ export class ProductService {
   private _albumUrl: string = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id:number) : Observable<Album>{
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
 }
